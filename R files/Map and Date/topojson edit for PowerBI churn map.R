e1c00fc83818b7a86df60b6a7c3bed4cb933fb8e
Ewan Reakes <123657625+ereakes@users.noreply.github.com>
2025-08-29 16:27:15 +0100

    Delete R files directory

--- R files/Map and Date/topojson edit for PowerBI churn map.R
+++
@@ -1,7 +0,0 @@
-library(geojsonio)
-library(tidyverse)
-
-topo <- topojson_read("county.topo.json")
-topo <- topo %>%
-  filter(str_detect(id, "^06"))
-topojson_write(topo, file = "CaliCounty.topo.json")
